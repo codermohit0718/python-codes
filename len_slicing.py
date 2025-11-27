f93e5103222ae42002f9115b09405f3cbcbd2b07
codermohit0718 <codermohit0718@gmail.com>
2025-11-27 22:20:44 +0530

    Add files via upload

---
+++ len_slicing.py
@@ -0,0 +1,8 @@
+name = "mohit"
+middle_name = "mahesh"
+last_name = "Patil"
+full_name = name+middle_name+last_name
+print("the name of person",full_name)
+print(len(full_name))
+print(full_name.count)
+print(full_name.capitalize)
